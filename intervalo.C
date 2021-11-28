2a2ee8285cc04967898b54bbe5d60c3c00a0e01c
ferdr <ferdr@LAPTOP-4G5VHA51.localdomain>
2021-11-28 02:06:59 +0100

    Intervalo exacto y mejoras esteticas

--- intervalo.C
+++ intervalo.C
@@ -5,14 +5,19 @@ double promedio(vector<double> datos,int n=10){
   return suma;
 }
 
-double mierda(vector<double> datos,int n=10){
+double mierda(vector<double> datos,int n=10){//sumas de las raices n+1-esimas del dato n
+  //perdon pero la que sugirieron con 10 datos agarra valores ~10^13, se complica
   double total=0;
   for(int i=0;i<n;i++)total = total+pow(datos[i],1.0/(i+1));
   return total;
 }
 
+double wilks(vector<double> datos,int n=10){
+  double Like = 0;
+  return Like;
+}
 
-void cinturon(double (*func)(vector<double>,int),string name, int n=10,int N=10000,bool debug=true){
+void cinturon(double (*func)(vector<double>,int),string name, int n=10,int N=100000,bool debug=false){
   string pdfName = string("intervalo_");
   pdfName += name;
   pdfName += string(".pdf");
@@ -32,151 +37,83 @@ void cinturon(double (*func)(vector<double>,int),string name, int n=10,int N=100
 
   auto c1 = new TCanvas("c1","Cinturon de confianza",700,1000);
   c1->Print(pdfBegin.c_str(),"pdf");
-  //auto hMierda = new TH1F("hMierda","",2000,8,40);
   double Highs[100];
   double Lows[100];
-  //double MierdaHighs[100];
-  //double MierdaLows[100];
   int iTau = 0;
   double Taus[100];
   for(double tau=0.1;tau<=10;tau=tau+0.1){
     Taus[iTau] = tau;
-    //hPromedio->Reset();
-    //hMierda->Reset();
-    
     for(int toy = 0; toy<N; toy++){  
-      double promedio = 0;
-      //double mierda = 0;
       for(int i = 0; i < n; i++){
         x[i] = r.Exp(tau);
-	if(debug&&N<20)cout<<x[i]<<endl;
-	//promedio = promedio + x[i];
-        //mierda = mierda + pow(x[i],1/(i+1));
       }
-      //promedio = promedio/n;
       stat[toy] = func(x,n);
-      if(debug&&N<20)cout<<stat[toy]<<endl;
 
     }
     //Mas lento pero asi tengo bin edges dinamicos
     std::sort(stat.begin(),stat.end());
-    auto h = new TH1F("h","",2000,stat[0]-0.1,stat[N-1]+0.1);
+    if(debug && N<20){
+      for(int i=0;i<N;i++)cout<<stat[i]<<endl;
+    }
+    //El histograma esta SOLO para plotear, el intervalo se saca exacto
+    auto h = new TH1F("h",name.c_str(),2000,stat[0]-0.1,stat[N-1]+0.1);
     for(int iStat = 0; iStat<N;iStat++)h->Fill(stat[iStat]);
-    //hMierda->Fill(mierda);
-    if(debug){
-      c1->Clear();
-      //c1->Divide(1,2);
-      //c1->cd(1);
-      h->Draw();
-      //c1->cd(2);
-      //hMierda->Draw();
-      c1->Print(pdfName.c_str(),"pdf");
-    }
+    
     //Find best interval
-    auto c = h->GetCumulative();
-    //auto cMierda = hMierda->GetCumulative();
-    c->Scale(1/h->Integral());
-    //cMierda->Scale(1/hMierda->Integral());
-    if(debug){
-      c1->Clear();
-      //c1->Divide(1,2);
-      //c1->cd(1);
-      c->Draw();
-      //c1->cd(2);
-      //cMierda->Draw();
-      c1->Print(pdfName.c_str(),"pdf");
-    }
-    int intDist = 1e6;
     double lowEdge;
     double highEdge;
-    //double lowEdgeMier;
-    //double highEdgeMier;
-    double binContent;
-    double mintofind;
-    int tempDist;
-    int lowBin;
-    for (int iBin = 1; iBin<c->GetNbinsX();iBin++){
-      binContent = c->GetBinContent(iBin);
-      if (binContent<0.68)continue;
-      mintofind = binContent-0.68;
-      for (int jBin = 1; jBin<iBin;jBin++){
-	if (c->GetBinContent(jBin)>mintofind){
-	  if (abs(c->GetBinContent(jBin)-mintofind)<abs(c->GetBinContent(jBin-1))){
-	    tempDist = iBin-jBin;
-	    lowBin = jBin;
-	  }
-	  else {
-	    tempDist = iBin-jBin+1;
-	    lowBin = jBin-1;
-	  }
-
-	break;
-	}
-      }
-      if (tempDist<intDist){
-	intDist = tempDist;
-        lowEdge = c->GetBinCenter(lowBin);
-        highEdge = c->GetBinCenter(iBin);
+    int distance = N*68/100;
+    double thisDist = 1e6;
+    for(int iStat=distance;iStat<N-1;iStat++){
+      double lowValue = stat[iStat-distance];
+      double highValue = stat[iStat];
+      if((highValue-lowValue)<thisDist){
+	thisDist = highValue-lowValue;
+	lowEdge = lowValue;
+	highEdge = highValue;
       }
     }
-    /*intDist = 1e6;
-    //if(debug)std::cout<<"Promedio: El borde bajo es "<<lowEdgeProm<<" y el alto es "<<highEdgeProm<<std::endl;
-    for (int iBin = 1; iBin<cMierda->GetNbinsX();iBin++){
-      binContent = cMierda->GetBinContent(iBin);
-      if (binContent<0.68)continue;
-      mintofind = binContent-0.68;
-      for (int jBin = 1; jBin<iBin;jBin++){
-	if (cMierda->GetBinContent(jBin)>mintofind){
-	  if (abs(cMierda->GetBinContent(jBin)-mintofind)<abs(cMierda->GetBinContent(jBin-1))){
-	    tempDist = iBin-jBin;
-	    lowBin = jBin;
-	  }
-	  else {
-	    tempDist = iBin-jBin+1;
-	    lowBin = jBin-1;
-	  }
-	break;
-	}
-      }
-      if (tempDist<intDist){
-	intDist = tempDist;
-        lowEdgeMier = cMierda->GetBinCenter(lowBin);
-        highEdgeMier = cMierda->GetBinCenter(iBin);
-      }
-    }*/
-    //if(debug)std::cout<<"Mierda: El borde bajo es "<<lowEdgeMier<<" y el alto es "<<highEdgeMier<<std::endl;
+    if(debug || r.Uniform(1)<0.05){
+      auto Interval = (TH1F*)h->Clone("Interval");
+      Interval->GetXaxis()->SetRangeUser(lowEdge,highEdge);
+      Interval->SetFillStyle(1001);
+      Interval->SetFillColor(2);
+      string hTitle = string("Tau = ");
+      hTitle += to_string(tau);
+      hTitle += ";";
+      hTitle += name;
+      hTitle += ";Entries";
+      h->SetTitle(hTitle.c_str());
+      h->Draw("hist");
+      Interval->Draw("same");
+      c1->Print(pdfName.c_str(),"pdf");
+      c1->Clear();
+    }
+    Lows[iTau] = lowEdge;
     Lows[iTau] = lowEdge;
     Highs[iTau] = highEdge;
-    //MierdaLows[iTau] = lowEdgeMier;
-    //MierdaHighs[iTau] = highEdgeMier;
     iTau++;
-    //if(debug)break;
   }
   auto gHighs = new TGraph(100,Highs,Taus);
   auto gLows = new TGraph(100,Lows,Taus);
-  //auto gMierdaHighs = new TGraph(100,MierdaHighs,Taus);
-  //auto gMierdaLows = new TGraph(100,MierdaLows,Taus);
   auto axis = gHighs->GetXaxis();
-  //auto axisMier = gMierdaHighs->GetXaxis();
   axis->SetLimits(Lows[0]-0.5,Highs[99]+0.5);
-  //axisMier->SetLimits(MierdaLows[0]-0.5,MierdaHighs[99]+0.5);
-  c1->Clear();
-  //c1->Divide(1,2);
-  //c1->cd(1);
-  gHighs->SetTitle(";Estadistico;#Tau");
+  string gTitle = string("Cinturon para estadistico ");
+  gTitle += name;
+  gTitle += string(";");
+  gTitle += name;
+  gTitle += string(";#tau");
+  gHighs->SetTitle(gTitle.c_str());
   gHighs->Draw();
   gLows->Draw("same");
-  //c1->cd(2);
-  //gMierdaHighs->SetTitle(";Mierda;#Tau");
-  //gMierdaHighs->Draw();
-  //gMierdaLows->Draw("same");
   c1->Print(pdfEnd.c_str(),"pdf");
- //TODO
- //Chequear cobertura
- //Hacer lo de -2LnLog(Like/OptLike)
-
+  c1->Clear();
+  //TODO
+  //Chequear cobertura
+  //Hacer lo de -2LnLog(Like/OptLike)
 }
 
 void intervalo(){
+  cinturon(&promedio,string("promedio"));
   cinturon(&mierda,string("mierda"));
 }
