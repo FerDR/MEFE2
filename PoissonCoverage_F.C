7a836a6abc939aa8b53ba5a750aa1eecbbce23fb
FerDR <fer.delrio.445@gmail.com>
2021-11-30 11:28:53 +0100

    Terminado entrega 3

--- PoissonCoverage_F.C
+++ PoissonCoverage_F.C
@@ -5,14 +5,23 @@ void PoissonCoverage_F(double mu_min = 0.0, double mu_max = 6.5, double CL = 0.6
 // Transformamos el CL en un número de sigmas de la Gaussiana N(0, 1)
 // esto es de utilidad para el cálculo de intervalos usando la varianza y la verosimilitud (ambos pendientes de implementar)
 
-// double nsigmas = ROOT::Math::gaussian_quantile((1+CL)/2,1);
-// double delta = nsigmas*nsigmas/2; 
+double nsigmas = ROOT::Math::gaussian_quantile((1+CL)/2,1);
+double delta = nsigmas*nsigmas/2; 
 
 // (1) Intervalo usando la varianza:  nobs +- sqrt(nobs) /////////////////////
-// A completar!
+auto IsInside1 = [&](double nobs, double mu){
+    return (nobs-nsigmas*pow(nobs,0.5)<=mu && mu<=nobs+nsigmas*pow(nobs,0.5));
+};
 
 // (2) Intervalo usando la verosimilitud: LL(ci_limits) = LLmax-1/2 ///////////
-// A completar!
+auto IsInside2 = [&](double nobs, double mu){
+    auto LL = new TF1("LL","[0]*log(x)-x",0,100.01);
+    LL->SetParameter(0,nobs);
+    double LLMax = LL->GetMaximum();
+    double muMin = (nobs==0)? 0 : LL->GetX(LLMax-delta,1e-4,nobs);
+    double muMax = (nobs==0)? delta : LL->GetX(LLMax-delta,nobs+1e-4,100);
+    return (muMin <= mu && mu <= muMax);
+};
 
 // (3) Intervalo frecuentista (analítico) ///////////////////////////////////
 auto IsInside3 = [&](double nobs, double mu) { 
@@ -22,11 +31,56 @@ auto IsInside3 = [&](double nobs, double mu) {
     return (xmin <= mu && mu <= xmax); 
 };
 
+
+// (4) Intervalo bayesiano ////////////////////////////
+auto IsInside4 = [&](double nobs, double mu) {
+    auto posterior = new TF1("posterior","[0]*TMath::PoissonI([1],x)",0,1000);
+    posterior->SetParameter(1,nobs);
+    posterior->SetParameter(0,1);
+    double norm = posterior->Integral(0,1000);
+    posterior->SetParameter(0,1./norm);
+    double xmin;
+    double xmax;
+    double muMax = posterior->GetMaximumX(0,1000);
+    //Solucion exacta para el caso n=0 para asegurarme que el minimo del intervalo no es el maximo de la funcion
+    if(nobs==0){
+      xmin=0;
+      xmax=-log(1-CL);
+    }
+    else{
+      double thisx;
+      double thisy;
+      double thisintegral;
+      double prevx;
+      double prevy;
+      double previntegral;
+      for(int i = 0; i < 100; i++){
+	thisx = muMax/100*(i+1);
+	thisy = posterior->Eval(thisx);
+	thisintegral = posterior->Integral(thisx,posterior->GetX(thisy,muMax,1000));
+	if(thisintegral>CL){
+	  prevx = thisx;
+	  prevy = thisy;
+	  previntegral = thisintegral;
+	  continue;
+	}
+	else{
+	  xmin = (abs(thisintegral-CL) < abs(previntegral-CL)) ? prevx : thisx;
+	  xmax = (abs(thisintegral-CL) < abs(previntegral-CL)) ? posterior->GetX(prevy,muMax,1000) : posterior->GetX(thisy,muMax,1000);
+	  break;
+        }
+      }
+    }
+    return (xmin <= mu && mu <= xmax);
+};
+
+
 ////////////////////////////////////////////////////////////////////////
 // Cobertura vs mu a ser guardadas en tres objetos de la clase TGraphs ////
-// TGraph * g1 = new TGraph(nscan_points);
-// TGraph * g2 = new TGraph(nscan_points);
+TGraph * g1 = new TGraph(nscan_points);
+TGraph * g2 = new TGraph(nscan_points);
 TGraph * g3 = new TGraph(nscan_points);
+TGraph * g4 = new TGraph(nscan_points);
 
 // Bucle sobre mu, desde mu_min a mu_max.
 // Para cada mu calcula la cobertura del intervalo y lo guarda en el TGraphs.
@@ -37,24 +91,33 @@ for (int i = 0; i < nscan_points; ++i) {
    int Nmax = int(mu+10*sqrt(mu))+1; // esperanza mas 10 veces sigma
 
    // Inicializar las variables que faltan
+   double probInside1 = 0;
+   double probInside2 = 0;
    double probInside3 = 0;
-   
+   double probInside4 = 0;
+
    // Barro desde n observado hasta la esperanza mas 10 veces sigma
    for (int nobs = 0; nobs < Nmax; ++nobs) {
 
       // Completar con lo correspondiente a los otros dos intervalos
-      
+      bool inside1 = IsInside1(nobs,mu);
+      bool inside2 = IsInside2(nobs,mu);
       bool inside3 = IsInside3(nobs,mu);
-      
+      bool inside4 = IsInside4(nobs,mu);
+
       // Calculo la probabilidad de obtener nobs de una Poisson con media mu
       double prob = ROOT::Math::poisson_pdf(nobs,mu); 
       // Si nobs esta dentro del intervalo, agrega la probabilidd de ese caso particular, i.e. Poisson(nobs,mu).
+      if (inside1) probInside1 += prob;
+      if (inside2) probInside2 += prob;
       if (inside3) probInside3 += prob;
+      if (inside4) probInside4 += prob;
    }
    
-   //g1->SetPoint(i,mu,probInside1);
-   //g2->SetPoint(i,mu,probInside2);
+   g1->SetPoint(i,mu,probInside1);
+   g2->SetPoint(i,mu,probInside2);
    g3->SetPoint(i,mu,probInside3);
+   g4->SetPoint(i,mu,probInside4);
    
 }
 
@@ -72,22 +135,36 @@ l->Draw(); // Draw a reference line at y-axis = CL
 // but first better look at each of them one at a time. Uncomment as necessary:
  
 // Naive Poisson interval (Red) ////////////////////////////////////////
-//  g1->SetLineWidth(2);
-//  g1->SetLineColor(kRed);
-//  g1->Draw("L");
-//  gPad->Update();
-//  gPad->WaitPrimitive();
+  g1->SetLineWidth(2);
+  g1->SetLineColor(kRed);
+  g1->Draw("L");
+  gPad->Update();
+  gPad->WaitPrimitive();
 
 // LogLikelihood interval (Blue line) //////////////////////////////////
-//  g2->SetLineWidth(2);
-//  g2->SetLineColor(kBlue);
-//  g2->Draw("L");
-//  gPad->Update();
-//  gPad->WaitPrimitive();
+  g2->SetLineWidth(2);
+  g2->SetLineColor(kBlue);
+  g2->Draw("L");
+  gPad->Update();
+  gPad->WaitPrimitive();
   
 // Analytic frequentist interval (Black line) //////////////////////////
   g3->SetLineWidth(2);
   g3->SetLineColor(kBlack);
   g3->Draw("L");
+  gPad->Update();
+  gPad->WaitPrimitive();
+
+  g4->SetLineWidth(2);
+  g4->SetLineColor(kGreen);
+  g4->Draw("L");
+
+  auto leg = new TLegend();
+  leg->AddEntry(g1,"Varianza");
+  leg->AddEntry(g2,"LogLikelihood");
+  leg->AddEntry(g3,"Frecuentista");
+  leg->AddEntry(g4,"Bayes");
+  leg->Draw("same");
+  gPad->Update();
 
 }
